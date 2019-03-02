703fd140362449aeb75db8f4f67289c7634b205f
Shiv Kumar <shiv.kmr003@gmail.com>
2019-03-02 16:04:04 -0500

    Initial Commit

---
+++ UrlLegalSlug/UrlLegalSlugTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UrlLegalSlugTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UrlLegalSlugTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a43f3b59-c93c-4661-9b41-bc3380c79e86")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UrlLegalSlug/UrlLegalSlugTests/SlugProducerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrlLegalSlug;
+
+namespace UrlLegalSlugTest
+{
+    [TestClass]
+    public class SlugProducerTests
+    {
+        [TestMethod]
+        [TestCategory("ClassTest")]
+        public void SlugProducer_GetUrlSlug_WhenUrlContainsNoIllegalCharacters_ReturnsAsIs()
+        {
+            // Arrange
+            var expectedSlug = "abcdegfhijklmnopqrstuvwxyz0123456789";
+
+            // Act
+            var actualSlug = SlugProducer.GetUrlSlug(expectedSlug);
+
+            // Assert
+            Assert.AreEqual(expectedSlug, actualSlug, $"We were expecting the Url slug to be: {expectedSlug}, but found the actual Url slug to be {actualSlug}");
+        }
+    }
+}
